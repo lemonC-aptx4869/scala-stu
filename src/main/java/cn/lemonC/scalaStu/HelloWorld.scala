ee6cb5841a0759dfb85e2b0252463a0452b292d8
lemonC-aptx4869 <1158651450@qq.com>
2024-05-23 23:51:53 +0800

    [add]：hello world

---
+++ src/main/java/cn/lemonC/scalaStu/HelloWorld.scala
@@ -0,0 +1,11 @@
+package cn.lemonC.scalaStu
+
+object HelloWorld {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+  }
+//  def main(args: Array[String]): Unit = {
+//    println("Hello World")
+//  }
+}
